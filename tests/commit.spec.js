679ef1d1774134f9dd1958576259efd36447bb04
wohu8292 <wohu8292@gmail.com>
2024-12-26 16:09:41 -0700

    annotation

--- tests/commit.spec.js
+++ tests/commit.spec.js
@@ -1,5 +1,18 @@
-const {expect, test} = require("@playwright/test")
+import { test, expect } from '@playwright/test';
 
-test("test", async({page})=>{
-    console.log("helloWorld")
-})
+test.describe('report tests', {
+  annotation: { type: 'category', description: 'report' },
+}, () => {
+  test('test report header', async ({ page }) => {
+    // ...
+  });
+
+  test('test full report', {
+    annotation: [
+      { type: 'issue', description: 'https://github.com/microsoft/playwright/issues/23180' },
+      { type: 'performance', description: 'very slow test!' },
+    ],
+  }, async ({ page }) => {
+    // ...
+  });
+});
